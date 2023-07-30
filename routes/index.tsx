896f7f89919e9514d829afaba11544ad20298d24
james-lane <4426937+james-lane@users.noreply.github.com>
2023-07-30 21:21:47 +0100

    Remove outer padding

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,7 +5,7 @@ import Hero from "../components/Hero.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="flex p-20 bg-gray-100 justify-center items-center flex-col w-screen h-screen">
+    <div class="flex bg-gray-100 justify-center items-center flex-col w-screen h-screen">
       <Head>
         <title>jameslane.co.uk</title>
       </Head>
